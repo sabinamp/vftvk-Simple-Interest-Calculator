8eb1a55f1d2d561e796d1a3b83b78715011f6af4
Sabina Maria <p_sabinamaria@yahoo.com>
2021-03-17 11:01:26 +0100

    js compute interest

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+
+
+
+/* document.getElementById("rate").oninput= function (){
+    let currentRate = document.getElementById("currentRate");
+    let  rate = document.getElementById("rate");
+    let sliderValue= rate.value;
+    currentRate.innerHTML = sliderValue;
+}; */
+
 function compute()
 {
-   let p = document.getElementById("principal").value;
-   let validData = checkdata();
+   let rate = document.getElementById("rate").value;
+   let amount= document.getElementById("principal").value;
+   let years = document.getElementById("years").value;
+  
+   let isValidData = checkdata();
+   if(isValidData){
+    let d= new Date();
+    let year = d.getFullYear()+ Number(years);
+    let interest = Number(rate)/100 * Number(amount) * Number(years);
+    let computedAmount = Number(amount) + interest;
+    let output=`<p>If you deposit ${amount} at an interest rate of ${rate},\n you will receive an amount of ${computedAmount}, in the year ${year}<p><br>`;
+    console.log("total amount after "+ years +" years is: "+computedAmount);
+    console.log("output:"+output);
+    document.getElementById("result").innerHTML= output;
+   }
     
 }
 
 const checkdata= ()=>{
-    let amount= document.getElementById("principal");
-    let years = document.getElementById("years");
-    let rate = document.getElementById("rate");
+    const amount= document.getElementById("principal");
+    const years = document.getElementById("years");
+   
     console.log("amount: "+amount.value +", years value:"+years.value +", interest rate:"+rate.value);
     //Check if amount field is empty
     if(amount.value == ""){
@@ -23,7 +46,9 @@ const checkdata= ()=>{
     return false;
     }
     //If all is well return true.
-    alert("Form validation is successful.")
+    //alert("Form validation is successful."+"\nYou entered the amount: "+amount.value +", years value:"+years.value +" and interest rate:"+rate.value);
     return true;
 }
-        
+
+
+     
