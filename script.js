8838b40e1f78abb0d73339c97d94e4d492f5762f
Sabina Maria <p_sabinamaria@yahoo.com>
2021-03-17 09:33:50 +0100

    update styles

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   let p = document.getElementById("principal").value;
+   
     
+}
+
+const checkdata= ()=>{
+    let amount= document.getElementById("principal");
+    let years = document.getElementById("years");
+    let rate = document.getElementById("rate");
+    //Check if amount field is empty
+    if(amount.value == ""){
+        alert("Please enter an amount");
+        amount.focus();
+        return false;
+    }
+    //Check if years field is empty
+    if(years.value == ""){
+    alert("Please enter a number of years");
+    years.focus();
+    return false;
+    }
+    //If all is well return true.
+    alert("Form validation is successful.")
+    return true;
 }
         
